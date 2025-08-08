a906d5c0f8fb404db295271c51d8e57630028185
Quyet881065 <nvquyet881065@gmail.com>
2025-08-09 02:05:06 +0700

    fix done file

--- src/data/songs.js
+++ src/data/songs.js
@@ -4,35 +4,35 @@ export const songs = [
         id:1,
         title : "Âm thầm bên em",
         artist : "Sơn Tùng MTP",
-        src : "/src/audio/amThamBenEm.mp3",
-        image : "/src/img/AmThamBenEm.jpg"
+        src : "/audio/amThamBenEm.mp3",
+        image : "/img/AmThamBenEm.jpg"
     },
     {
         id:1,
         title : "Mất kết nối",
         artist : "Dương Momic",
-        src : "/src/audio/matKetNoi.mp3",
-        image : "/src/img/anhsems.jpg"
+        src : "/audio/matKetNoi.mp3",
+        image : "/img/anhsems.jpg"
     },
     {
         id:3,
         title: "Vẫn nhớ",
         artist : "Tuấn Hưng",
-        src:"/src/audio/vanNho.mp3",
-        image : "/src/img/cuoithoi.jpg"
+        src:"/audio/vanNho.mp3",
+        image : "/img/cuoithoi.jpg"
     },
     {
         id:4,
         title: "Chỉ muốn bên em lúc này",
         artist : "Thoại Nghi",
-        src:"/src/audio/chiMuonBenEmLucNay.mp3",
-        image : "/src/img/PayPhone.jpg"
+        src:"/audio/chiMuonBenEmLucNay.mp3",
+        image : "/img/PayPhone.jpg"
     },
     {
         id:5,
         title: "Say sóng",
         artist : "Tăng Nhật Tuệ",
-        src:"/src/audio/saySong.mp3",
-        image : "/src/img/TNOAL.jpg"
+        src:"/audio/saySong.mp3",
+        image : "/img/TNOAL.jpg"
     },
 ]
